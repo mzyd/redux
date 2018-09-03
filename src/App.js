4720f2a8cdd42332cfe8a94b3624f3c50142d3d6
mzy <mzy3991@gmail.com>
2018-09-03 13:18:38 +0800

    1. 妙用 bindActionCreators 2. 使用修饰器

--- src/App.js
+++ src/App.js
@@ -3,10 +3,29 @@ import './App.css';
 import { Button } from 'react-bootstrap'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { increment, decrement } from './actions'
+/* import { increment, decrement } from './actions' */
+import * as types from './actions'
 import { bindActionCreators } from 'redux'
 
+const mapStateToProps = (state) => {
+  return {
+    counter: state.counter
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators(types, dispatch)
+}
+
+@testable
+@connect(mapStateToProps, mapDispatchToProps)
 class App extends Component {
+
+  static propTypes = {
+    counter: PropTypes.number.isRequired,
+    increment: PropTypes.func.isRequired,
+    decrement: PropTypes.func.isRequired,
+  }
   render() {
     /* const { dispatch } = this.props */
     console.log( "props: ", this.props )
@@ -29,17 +48,11 @@ class App extends Component {
   }
 }
 
-App.propTypes = {
-  counter: PropTypes.number.isRequired,
-  increment: PropTypes.func.isRequired,
-  decrement: PropTypes.func.isRequired,
+function testable(target) {
+  target.isTestable = true;
 }
 
-const mapStateToProps = (state) => {
-  return {
-    counter: state.counter
-  }
-}
+console.log( "-testable-", App.isTestable )
 
 /* const mapDispatchToProps = (dispatch) => {
  *   return {
@@ -47,12 +60,14 @@ const mapStateToProps = (state) => {
  *   }
  * } */
 
-/* const mapDispatchToProps = (dispatch) => {
- *   return bindActionCreators({ increment }, dispatch)
- * } */
+// Action 很多的时候使用 bindActionCreators
+
 
 /* export default connect(mapStateToProps, mapDispatchToProps)(App); */
-export default connect(
-  mapStateToProps,
-  { increment, decrement }
-)(App);
+
+/* export default connect(
+ *   mapStateToProps,
+ *   mapDispatchToProps
+ * )(App); */
+
+export default App
